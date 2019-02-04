70c7bbb8839920ec45ebc6ac3886649eaa115df1
AquaticNewKeyHack <aaronquaday@gmail.com>
2019-02-04 14:31:34 -0800

    *rf* refactored to deploy masterRecipeFragment on activity_main, *rn* renamed prev. frag xml container to be ready to host a recipe

--- app/src/main/java/com/aaqanddev/aaqsbakingapp/MainActivity.java
+++ app/src/main/java/com/aaqanddev/aaqsbakingapp/MainActivity.java
@@ -2,26 +2,11 @@ package com.aaqanddev.aaqsbakingapp;
 
 
 import android.os.Bundle;
-import android.util.Log;
 
-import com.aaqanddev.aaqsbakingapp.model.Recipe;
-import com.aaqanddev.aaqsbakingapp.util.RecipeAdapter;
-import com.aaqanddev.aaqsbakingapp.util.RetroInterface;
-import com.aaqanddev.aaqsbakingapp.util.RetroService;
 import com.aaqanddev.aaqsbakingapp.view.RecipeListFragment;
-import com.google.gson.Gson;
-
-import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,14 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //I guess that onSavedInstanceState of a hosted fragment
+        //automatically is checked/preserved with onCreate(savedInstanceState)
+        if (savedInstanceState == null){
+        /* TODO refactor this code to use in detailsView (fragment_recipe.xml)
+            RecipeListFragment recipeFrag = new RecipeListFragment();
 
-        RecipeListFragment recipeFrag = new RecipeListFragment();
+            FragmentManager fragMangr = getSupportFragmentManager();
 
-        FragmentManager fragMangr = getSupportFragmentManager();
-
-        fragMangr.beginTransaction()
-                .add(R.id.recipes_container, recipeFrag)
-                .commit();
+            fragMangr.beginTransaction()
+                    .add(R.id.recipes_container, recipeFrag)
+                    .commit();
+        */
+        }
 
     }
 }
